7ada1d1e2cf6a7caa08f7d1b4c1d454d350efe51
Alekseev Aleksey <hell1k@yandex.ru>
2023-10-30 11:40:36 +0700

    *

--- tests/TestTest.py
+++ tests/TestTest.py
@@ -1,6 +1,11 @@
+import pytest
+
+
 class TestTest:
+    @pytest.mark.test2
     def test_some_test(self, setup):
         assert 2 == 2
 
+    @pytest.mark.test2
     def test_some_test2(self, setup):
         assert 2 == 3
